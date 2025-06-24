ade90616784aab1c6a0a632a682ff0eb999ff791
DyrenJmsu <darenjamesdedios@gmail.com>
2025-06-24 18:47:44 +0800

    Planning to use ttk to let the user modify their preferred theme.

---
+++ themes.py
@@ -0,0 +1,6 @@
+#Planning to add light and dark theme for the user to choose their preferred theme.
+from tkinter import ttk
+
+class ThemeManager:
+    def __init__(self):
+        self.style = ttk.Style()
